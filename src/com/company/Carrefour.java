80a4ff44412f50284b6f6b88dec3f86897688555
ChristianHiroz <twinkie.hiroz@gmail.com>
2014-10-09 17:37:26 +0200

    Carrefour class added

--- src/com/company/Carrefour.java
+++ src/com/company/Carrefour.java
@@ -1,12 +1,8 @@
 package com.company;
 
-import java.util.ArrayList;
-
 /**
  * Created by Pony on 09/10/2014. With IntelliJ
  */
-public class Ville {
-    public Ville(){
+public class Carrefour {
 
-    }
 }

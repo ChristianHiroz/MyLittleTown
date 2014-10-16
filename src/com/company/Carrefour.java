f16ce64b63414b57733a170b44e04a6b7ac6d63b
axeleroy <Clayr0sp33D>
2014-10-16 14:27:57 +0200

    Implémentation du Carrefour

--- src/com/company/Carrefour.java
+++ src/com/company/Carrefour.java
@@ -4,5 +4,21 @@ package com.company;
  * Created by Pony on 09/10/2014. With IntelliJ
  */
 public class Carrefour {
+    private CircularLinkedList sorties;
 
+    public Carrefour() {
+        sorties = new CircularLinkedList();
+    }
+
+    public void addRoute(Route route) {
+        sorties.insertAfterActual(route);
+    }
+
+    public Route turn() {
+        do {
+            sorties.goToNextElement();
+        }while (!((Route)sorties.getActualElementData()).isOccupe());
+
+        return (Route)sorties.getActualElementData();
+    }
 }

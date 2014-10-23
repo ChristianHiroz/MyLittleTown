51ad36384b1af38f32a4a317ebd5de8a824526e0
ChristianHiroz <twinkie.hiroz@gmail.com>
2014-10-23 14:08:55 +0200

    Route created (extend from abstact)

--- src/com/company/Route.java
+++ src/com/company/Route.java
@@ -1,66 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by axeleroy on 09/10/2014.
- * Modèle de donnée représentant une portion de route
+ * Created by Pony on 23/10/2014. With IntelliJ
  */
-public class Route {
-    public int posX; // Utilisé pour représentation graphique
-    public int posY; // Utilisé pour représentation graphique
-    private boolean occupe;
-    public Route nextPortion; // Le ou les portions de route suivante(s).
+public class Route extends AbstractRoute {
+    List<PortionRoute> route;
 
-    public Route(int xPos, int yPos) {
-        this.posX = xPos;
-        this.posY = yPos;
-        occupe = false;
-        nextPortion = null;
+    public Route(List<PortionRoute> route) {
+        this.route = new ArrayList<PortionRoute>();
     }
 
-    public boolean isOccupe() {
-        return occupe;
+    public List<PortionRoute> getRoute() {
+        return route;
     }
 
-    public synchronized void occuperRoute() {
-        occupe = true;
-    }
-
-    public synchronized void liberRoute() {
-        occupe = false;
-    }
-
-    public synchronized void setNextPortion(Route route) {
-        nextPortion = route;
-    }
-
-    public synchronized Route getNextPortion() {
-        return nextPortion;
-    }
-
-    /* Fonction pour connaître si une portion est égale à une autre
-    * Utile si stockée dans un Set
-    */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Route route = (Route) o;
-
-        if (occupe != route.occupe) return false;
-        if (posX != route.posX) return false;
-        if (posY != route.posY) return false;
-        if (nextPortion != null ? !nextPortion.equals(route.nextPortion) : route.nextPortion != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = posX;
-        result = 31 * result + posY;
-        result = 31 * result + (occupe ? 1 : 0);
-        result = 31 * result + (nextPortion != null ? nextPortion.hashCode() : 0);
-        return result;
+    public void setRoute(List<PortionRoute> route) {
+        this.route = route;
     }
 }

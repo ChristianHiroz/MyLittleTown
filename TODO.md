9ed592e7ea012f76794ad401682096060caadb3a
ChristianHiroz <twinkie.hiroz@gmail.com>
2014-11-06 14:27:41 +0100

    Add face + TODO list

---
+++ TODO.md
@@ -0,0 +1,8 @@
+MyLittleTown
+============
+
+- Class face
+- Revoir Carrefour
+- Refaire la class voiture
+- Thread sur les voitures
+- Ville factory (génération de n villes, voir aléatoire?)
---
+++ src/com/company/Face.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * Created by Fluttershy on 06/11/2014.
+ */
+public class Face {
+    private Portion entree;
+    private Portion sortie;
+
+    public Face(Portion entree, Portion sortie) {
+        this.entree = entree;
+        this.sortie = sortie;
+    }
+
+    public Portion getEntree() {
+        return entree;
+    }
+
+    public void setEntree(Portion entree) {
+        this.entree = entree;
+    }
+
+    public Portion getSortie() {
+        return sortie;
+    }
+
+    public void setSortie(Portion sortie) {
+        this.sortie = sortie;
+    }
+
+    public void connect(Face cacher){
+        cacher.getSortie().setSuivant(this.getEntree());
+        this.getSortie().setSuivant(cacher.getEntree());
+    }
+}

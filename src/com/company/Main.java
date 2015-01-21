999790c8cd8047b71ba5048bdb211c0d21c8e3b0
axel <blackeco@free.fr>
2015-01-21 19:56:44 +0100

    Main.java:  - Ajout d'une troisième voiture.  - Renommage de la variable gestionnaireFeux.  - Ajout de commentaires et réorganisation du code.  - TODO: Renommer les rues ?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         RouteFactory rf = RouteFactory.getInstance();
+        GestionnaireFeux gestionnaireFeux = new GestionnaireFeux();
 
         Route r0 = rf.createRoute(5, "Avenue du Lapin Doré");
         Route r1 = rf.createRoute(5, "Rue du PorteJartelle");
@@ -17,29 +18,33 @@ public class Main {
         Route r6 = rf.createRoute(5, "Quai du Phalus");
         Route r7 = rf.createRoute(5, "Chemin de la Fin du Monde");
 
+        // Création du premier carré de routes
         r0.getFace(0).connect(r1.getFace(1));
         r1.getFace(0).connect(r2.getFace(1));
         r2.getFace(0).connect(r3.getFace(1));
-//        r3.getFace(0).connect(r0.getFace(1));
+
+
+        // Création du second carré de routes
         r4.getFace(0).connect(r5.getFace(1));
         r5.getFace(0).connect(r6.getFace(1));
         r6.getFace(0).connect(r7.getFace(1));
 
+        // Listing des faces qu'on va lier afin de créer une intersection
         List<Face> faces = new ArrayList<Face>();
         faces.add(r0.getFace(1));
         faces.add(r3.getFace(0));
         faces.add(r4.getFace(1));
         faces.add(r7.getFace(0));
 
-        GestionnaireFeux g = new GestionnaireFeux();
-        Intersection i0 = new Intersection(faces, g);
-
-        g.start();
+        Intersection i0 = new Intersection(faces, gestionnaireFeux);
+        gestionnaireFeux.start();
 
         Voiture v0 = new Voiture(r0.getEntree(0), Comportement.CHAUFFARD, "Gerard");
         Voiture v1 = new Voiture(r0.getEntree(0).getSuivants().get(0), Comportement.TOURISTE, "Jacky");
+        Voiture v3 = new Voiture(r4.getEntree(1), Comportement.LIVREUR, "Bernard");
         v0.start();
         v1.start();
+        v3.start();
 
         Thread.sleep(120000);
         System.out.println("End of prog... Exiting");
